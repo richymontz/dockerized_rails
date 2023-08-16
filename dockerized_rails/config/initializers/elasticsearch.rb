8e5a514a27d4ee6fe84b86a0dffcee06178f02db
Your Name <ingricardomontes@gmail.com>
2023-08-16 15:11:59 -0500

    update elasticsearch initializer

--- dockerized_rails/config/initializers/elasticsearch.rb
+++ dockerized_rails/config/initializers/elasticsearch.rb
@@ -1,12 +1,4 @@
-config = {
-  transport_options: { request: { timeout: 5 } }
-}
-
-if File.exist?('config/elasticsearch.yml')
-  template = ERB.new(File.new('config/elasticsearch.yml').read)
-  processed = YAML.safe_load(template.result(binding))
-  config.merge!(processed[Rails.env].symbolize_keys)
-end
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(config)
+Elasticsearch::Model.client = Elasticsearch::Client.new(
+  url: ENV['ELASTICSEARCH_URL'],
+  retry_on_failure: 5
+)
